a66b404465d2347a32bb116d0cbd8b02ab481fef
Mikhael Jude M. Dionio <mikhaeldionio@gmail.com>
2017-12-09 17:10:42 +0800

    adds a few more links to better connect the pages

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from . import views
+
+app_name = 'home'
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^signup/$',views.signup,name='signup'),
